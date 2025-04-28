91c5bba5b3327ad57ff9736594ffc1405e8c3ed2
kimura79 <ema.saracino@gmail.com>
2025-04-28 10:43:39 +0200

    Update landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -57,7 +57,7 @@ export default function Landing() {
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
                     <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-indigo-400">
-  <i className="fas fa-book"></i>
+  <i className="fas fa-file-alt"></i>
 </div>
                     <h6 className="text-xl font-semibold">{t('feature_3_title')}</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">{t('feature_3_desc')}</p>
