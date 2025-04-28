c46be3a90a81861f1b0a38c61749bec53fdf5d04
kimura79 <ema.saracino@gmail.com>
2025-04-28 11:49:23 +0200

    Update Card.js

--- components/Cards/Card.js
+++ components/Cards/Card.js
@@ -1,7 +1,6 @@
-// components/Cards/Card.js
 export default function Card({ children }) {
   return (
-    <div className="bg-white rounded-xl shadow-lg p-6">
+    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-2xl p-6 transition-all duration-300 hover:shadow-2xl">
       {children}
     </div>
   );
